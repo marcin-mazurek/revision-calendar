fe03805dc81321d1ff3ccae75bef74dc7f24047e
Marcin Mazurek <marcin@mazurek.pro>
2016-03-22 17:43:28 +0000

    Add Polymer

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use('/', express.static('static'));
+app.use('/bower_components', express.static('bower_components'));
 
 app.listen(3000, function () {
   console.log('Server running on port 3000.');
