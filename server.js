0c3d7179bb8e9bd729513ae8ca073501b1a92d73
Marcin Mazurek <marcin@mazurek.pro>
2017-02-05 11:43:34 +0100

    Add environment variable-based config

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const port = (process.env.PORT || 3000);
 
+app.get('/config.js', (req, res) => {
+  const config = {
+    clientId: process.env.CLIENT_ID || null,
+    days: process.env.DAYS ? process.env.DAYS.split(',') : [3, 10, 30, 60]
+  };
+
+  res.send(`const config = ${JSON.stringify(config)};`);
+});
 app.use('/', express.static('static'));
 app.use('/bower_components', express.static('bower_components'));
 
